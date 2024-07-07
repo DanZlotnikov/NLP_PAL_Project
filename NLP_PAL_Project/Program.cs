4d2a0870fa03e197e634487deaff50accee3d8d0
denis <panor@post.bgu.ac.il>
2024-07-07 13:01:06 +0300

    added timeout stats

--- NLP_PAL_Project/Program.cs
+++ NLP_PAL_Project/Program.cs
@@ -9,9 +9,9 @@ namespace NLP_PAL_Project
         {
             Consts.Init();
 
-            (int correct, int incorrect, int error) pythonStats = (0, 0, 0);
-            (int correct, int incorrect, int error) javaScriptStats = (0, 0, 0);
-            (int correct, int incorrect, int error) rubyStats = (0, 0, 0);
+            (int correct, int incorrect, int error, int timeout) pythonStats = (0, 0, 0, 0);
+            (int correct, int incorrect, int error, int timeout) javaScriptStats = (0, 0, 0, 0);
+            (int correct, int incorrect, int error, int timeout) rubyStats = (0,0, 0, 0);
             int total = 0;
 
             List<QuestionObj> questionObjs = await Gsm8k.ReadQuestionsFromJsonAsync($"../../../{Consts.config["GSM"]["FilePath"].ToString()}");
@@ -21,21 +21,23 @@ namespace NLP_PAL_Project
             foreach (QuestionObj questionObj in questionObjs)
             {
                 CodeExecutor codeExecutor = new CodeExecutor();
-                (double answer, Boolean error) PythonOutput = await codeExecutor.ExecutePythonCode(questionObj.LanguageObjects[Language.Python].GeneratedAnswer);
-                (double answer, Boolean error) RubyOutput = await codeExecutor.ExecuteRubyCode(questionObj.LanguageObjects[Language.Ruby].GeneratedAnswer);
-                (double answer, Boolean error) JSOutput = await codeExecutor.ExecuteJavaScriptCode(questionObj.LanguageObjects[Language.JS].GeneratedAnswer);
+                (double answer, Boolean error, Boolean timeout) PythonOutput = await codeExecutor.ExecutePythonCode(questionObj.LanguageObjects[Language.Python].GeneratedAnswer);
+                (double answer, Boolean error, Boolean timeout) RubyOutput = await codeExecutor.ExecuteRubyCode(questionObj.LanguageObjects[Language.Ruby].GeneratedAnswer);
+                (double answer, Boolean error, Boolean timeout) JSOutput = await codeExecutor.ExecuteJavaScriptCode(questionObj.LanguageObjects[Language.JS].GeneratedAnswer);
 
-                pythonStats = (PythonOutput.error ? (pythonStats.correct, pythonStats.incorrect, pythonStats.error + 1)
-                    : PythonOutput.Item1 == questionObj.RealAnswer ? (pythonStats.correct + 1, pythonStats.incorrect, pythonStats.error)
-                    : (pythonStats.correct, pythonStats.incorrect + 1, pythonStats.error));
-                rubyStats = (RubyOutput.error ? (rubyStats.correct, rubyStats.incorrect, rubyStats.error + 1)
-                    : RubyOutput.Item1 == questionObj.RealAnswer ? (rubyStats.correct + 1, rubyStats.incorrect, rubyStats.error)
-                    : (rubyStats.correct, rubyStats.incorrect + 1, rubyStats.error));
-                javaScriptStats = (JSOutput.error ? (javaScriptStats.correct, javaScriptStats.incorrect, javaScriptStats.error + 1)
-                    : JSOutput.Item1 == questionObj.RealAnswer ? (javaScriptStats.correct + 1, javaScriptStats.incorrect, javaScriptStats.error)
-                    : (javaScriptStats.correct, javaScriptStats.incorrect + 1, javaScriptStats.error));
+                pythonStats = PythonOutput.timeout ? (pythonStats.correct, pythonStats.incorrect, pythonStats.error + 1,pythonStats.timeout)
+                    : (PythonOutput.error ? (pythonStats.correct, pythonStats.incorrect, pythonStats.error + 1,pythonStats.timeout)
+                    : PythonOutput.Item1 == questionObj.RealAnswer ? (pythonStats.correct + 1, pythonStats.incorrect, pythonStats.error,pythonStats.timeout)
+                    : (pythonStats.correct, pythonStats.incorrect + 1, pythonStats.error,pythonStats.timeout));
+                rubyStats = RubyOutput.timeout ? (rubyStats.correct, rubyStats.incorrect, rubyStats.error + 1,rubyStats.timeout)
+                    : (RubyOutput.error ? (rubyStats.correct, rubyStats.incorrect, rubyStats.error + 1,rubyStats.timeout)
+                    : RubyOutput.Item1 == questionObj.RealAnswer ? (rubyStats.correct + 1, rubyStats.incorrect, rubyStats.error,rubyStats.timeout)
+                    : (rubyStats.correct, rubyStats.incorrect + 1, rubyStats.error,rubyStats.timeout));
+                javaScriptStats = JSOutput.timeout ? (javaScriptStats.correct, javaScriptStats.incorrect, javaScriptStats.error + 1,javaScriptStats.timeout)
+                    : (JSOutput.error ? (javaScriptStats.correct, javaScriptStats.incorrect, javaScriptStats.error + 1,javaScriptStats.timeout)
+                    : JSOutput.Item1 == questionObj.RealAnswer ? (javaScriptStats.correct + 1, javaScriptStats.incorrect, javaScriptStats.error,javaScriptStats.timeout)
+                    : (javaScriptStats.correct, javaScriptStats.incorrect + 1, javaScriptStats.error,javaScriptStats.timeout));
                 total++;
-
             }
             Console.WriteLine($"python: {pythonStats}; js: {javaScriptStats}; ruby: {rubyStats}");
             Console.ReadLine();
